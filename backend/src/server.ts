06628363acc45e54e8d436fef15e570160dbd5ff
Lapidis2 <hitlapidis2@gmail.com>
2026-02-07 09:36:36 +0200

    created user routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
-
+import authRoutes from "./routes/user.routes";
+import postRoutes from "./routes/post.routes";
 
 dotenv.config();
 connectDB();
@@ -13,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
